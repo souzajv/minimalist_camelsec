e7d13fbee0eec79f88ac91cc56161bda5bdbad97
João <joovictorsouza5@gmail.com>
2025-03-19 12:11:55 -0300

    feat: add loading screen

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 
+import { useState } from "react";
+import Loading from "./loading";
 import BlobCursor from "./componets/blobcursor/blobcursor";
 import Squares from "./componets/squares/squares";
 import Header from "./componets/header/header";
 import Title from "./componets/title/title";
 import Globe from "./componets/globe/globe";
-// import Subtitle from "./componets/subtitle/subtitle";
-// 1import Button from "./componets/button/button";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true); 
 
   return (
-    <main>
-      <Squares />
-      <Header />
-      <section>
-        <Title />
-      </section>
-      <Globe />
-      <BlobCursor />
-    </main>
+    <>
+      {loading ? ( 
+        <Loading onComplete={() => setLoading(false)} />
+      ) : (
+        <main>
+          <Squares />
+          <Header />
+          <section>
+            <Title />
+          </section>
+          <Globe />
+          <BlobCursor />
+        </main>
+      )}
+    </>
   );
 }
